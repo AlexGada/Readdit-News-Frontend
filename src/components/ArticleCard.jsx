55f04e6a99dd16a7dadd76c616d533505b67873b
Alex Gada <alexgada01@gmail.com>
2021-05-12 17:18:24 +0100

    article card styling

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,68 +1,76 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import { Link } from "@reach/router";
 import Voter from "./Voter";
 import { formatDate } from "../utils/utils";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
 export default function ArticleCard(props) {
-  const classes = useStyles();
-  console.log(props.article);
-  // const bull = <span className={classes.bullet}>•</span>;
-  // {article} = this.props
   return (
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          {props.article.topic}
-        </Typography>
+    <div className="ArticleCard" key={props.article.title}>
+      <div className="ArticleCardMain">
         <Link to={`/articles/${props.article.article_id}`}>
-          <Typography variant="h5" component="h2">
-            <h2>{props.article.title}</h2>
-          </Typography>
+          <h2>{props.article.title}</h2>
         </Link>
-        <Typography className={classes.pos} color="textSecondary">
-          by {props.article.author}
-        </Typography>
+        <p>
+          <b>{props.article.topic}</b> | posted by{" "}
+          <em>{props.article.author}</em>
+          <br />
+          {formatDate(props.article.created_at)}
+        </p>
+      </div>
+      <div className="ArticleCardSmall">
         <Voter
           key={`vot${props.article.article_id}`}
           votes={props.article.votes}
           article_id={props.article.article_id}
         />
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
+        <Link to={`/articles/${props.article.article_id}`}>
           {props.article.comment_count} comments <br></br>
-          posted {formatDate(props.article.created_at)}
-        </Typography>
-      </CardContent>
-    </Card>
+        </Link>
+      </div>
+    </div>
   );
 }
+
+//   <Card className={classes.root} variant="outlined">
+//    <CardContent>
+//      <Typography className={classes.title} color="textSecondary" gutterBottom>
+//        {props.article.topic}
+//      </Typography>
+//      <Link to={`/articles/${props.article.article_id}`}>
+//        <Typography variant="h5" component="h2">
+//          <h2>{props.article.title}</h2>
+//        </Typography>
+//      </Link>
+//      <Typography className={classes.pos} color="textSecondary">
+//        by {props.article.author}
+//      </Typography>
+//      <Voter
+//        key={`vot${props.article.article_id}`}
+//        votes={props.article.votes}
+//        article_id={props.article.article_id}
+//      />
+//      <Typography className={classes.title} color="textSecondary" gutterBottom>
+//        {props.article.comment_count} comments <br></br>
+//        posted {formatDate(props.article.created_at)}
+//      </Typography>
+//    </CardContent>
+//  </Card>
+
+// const useStyles = makeStyles({
+//   root: {
+//     minWidth: 275,
+//   },
+//   bullet: {
+//     display: "inline-block",
+//     margin: "0 2px",
+//     transform: "scale(0.8)",
+//   },
+//   title: {
+//     fontSize: 14,
+//   },
+//   pos: {
+//     marginBottom: 12,
+//   },
+// });
+//   );
+// }
